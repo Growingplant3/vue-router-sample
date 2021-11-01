73667c47bc8daeebada5bac7a9e2a2aa63ad89c1
Yoshinori Shimoda <ys@platinum.local>
2021-11-02 01:04:50 +0900

    2021/11/2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,24 +9,34 @@
       @click='toUsers'
     >Usersのページに行く</button>
     <p>{{ count }}</p>
-    <p>{{ doublecount }}</p>
+    <p>{{ doubleCount }}</p>
     <p>{{ tripleCount }}</p>
+    <!-- <p>{{ myComponentDoubleCount }}</p> -->
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
+  // computed: {
+  //   count() {
+  //     return this.$store.state.count;
+  //   },
+  //   doublecount() {
+  //     // return this.$store.state.count * 2;
+  //     return this.$store.getters.doubleCount;
+  //   },
+  //   tripleCount() {
+  //     return this.$store.getters.tripleCount;
+  //   }
+  // },
+  // computed: mapGetters(['doubleCount', 'tripleCount']), // mapGettersで呼び出せる
+  // computed: mapGetters({
+  //   myComponentDoubleCount: 'doubleCount' // キー名を変更できる
+  // }),
   computed: {
-    count() {
-      return this.$store.state.count;
-    },
-    doublecount() {
-      // return this.$store.state.count * 2;
-      return this.$store.getters.doubleCount;
-    },
-    tripleCount() {
-      return this.$store.getters.tripleCount;
-    }
+    ...mapGetters(['doubleCount', 'tripleCount']) // スプレッド演算子
   },
   methods: {
     toUsers() {
